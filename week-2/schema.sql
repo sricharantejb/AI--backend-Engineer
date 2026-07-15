32f94a688ee5b1939209fd2cb7e8a1b9a835ec27
Sri Charan Tej <sricharantejb@gmail.com>
2026-07-15 21:31:26 +0530

    Delete week-2 directory

--- week-2/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE students (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    age INT
-);
--- week-2/service.py
+++
@@ -1,6 +0,0 @@
-from repository import StudentRepository
-
-repo = StudentRepository()
-
-def get_students():
-    return repo.get_students()
